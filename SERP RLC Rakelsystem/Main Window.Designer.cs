bec1fad16cf06b47b16ab72762760a4a50cf3a84
Anton Andersson <anton.andersson32@learnet.se>
2020-09-30 21:19:46 +0200

    Initial Commit

---
+++ SERP RLC Rakelsystem/Main Window.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.anropbtn = new System.Windows.Forms.Button();
+            this.raps50btn = new System.Windows.Forms.Button();
+            this.raps51btn = new System.Windows.Forms.Button();
+            this.raps52btn = new System.Windows.Forms.Button();
+            this.raps53btn = new System.Windows.Forms.Button();
+            this.raps54btn = new System.Windows.Forms.Button();
+            this.raps55btn = new System.Windows.Forms.Button();
+            this.insats51btn = new System.Windows.Forms.Button();
+            this.insats52btn = new System.Windows.Forms.Button();
+            this.insats53btn = new System.Windows.Forms.Button();
+            this.insats54btn = new System.Windows.Forms.Button();
+            this.channelDisplay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // anropbtn
+            // 
+            this.anropbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.anropbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.anropbtn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.anropbtn.ForeColor = System.Drawing.Color.White;
+            this.anropbtn.Location = new System.Drawing.Point(23, 68);
+            this.anropbtn.Name = "anropbtn";
+            this.anropbtn.Size = new System.Drawing.Size(388, 48);
+            this.anropbtn.TabIndex = 0;
+            this.anropbtn.Text = "ANROP STOCKHOLM";
+            this.anropbtn.UseVisualStyleBackColor = false;
+            // 
+            // raps50btn
+            // 
+            this.raps50btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.raps50btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.raps50btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raps50btn.ForeColor = System.Drawing.Color.White;
+            this.raps50btn.Location = new System.Drawing.Point(23, 122);
+            this.raps50btn.Name = "raps50btn";
+            this.raps50btn.Size = new System.Drawing.Size(191, 48);
+            this.raps50btn.TabIndex = 1;
+            this.raps50btn.Text = "RAPS 50";
+            this.raps50btn.UseVisualStyleBackColor = false;
+            // 
+            // raps51btn
+            // 
+            this.raps51btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.raps51btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.raps51btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raps51btn.ForeColor = System.Drawing.Color.White;
+            this.raps51btn.Location = new System.Drawing.Point(220, 122);
+            this.raps51btn.Name = "raps51btn";
+            this.raps51btn.Size = new System.Drawing.Size(191, 48);
+            this.raps51btn.TabIndex = 2;
+            this.raps51btn.Text = "RAPS 51";
+            this.raps51btn.UseVisualStyleBackColor = false;
+            // 
+            // raps52btn
+            // 
+            this.raps52btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.raps52btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.raps52btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raps52btn.ForeColor = System.Drawing.Color.White;
+            this.raps52btn.Location = new System.Drawing.Point(23, 176);
+            this.raps52btn.Name = "raps52btn";
+            this.raps52btn.Size = new System.Drawing.Size(191, 48);
+            this.raps52btn.TabIndex = 3;
+            this.raps52btn.Text = "RAPS 52";
+            this.raps52btn.UseVisualStyleBackColor = false;
+            // 
+            // raps53btn
+            // 
+            this.raps53btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.raps53btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.raps53btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raps53btn.ForeColor = System.Drawing.Color.White;
+            this.raps53btn.Location = new System.Drawing.Point(220, 176);
+            this.raps53btn.Name = "raps53btn";
+            this.raps53btn.Size = new System.Drawing.Size(191, 48);
+            this.raps53btn.TabIndex = 4;
+            this.raps53btn.Text = "RAPS 53";
+            this.raps53btn.UseVisualStyleBackColor = false;
+            // 
+            // raps54btn
+            // 
+            this.raps54btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.raps54btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.raps54btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raps54btn.ForeColor = System.Drawing.Color.White;
+            this.raps54btn.Location = new System.Drawing.Point(23, 230);
+            this.raps54btn.Name = "raps54btn";
+            this.raps54btn.Size = new System.Drawing.Size(191, 48);
+            this.raps54btn.TabIndex = 5;
+            this.raps54btn.Text = "RAPS 54";
+            this.raps54btn.UseVisualStyleBackColor = false;
+            // 
+            // raps55btn
+            // 
+            this.raps55btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.raps55btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.raps55btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.raps55btn.ForeColor = System.Drawing.Color.White;
+            this.raps55btn.Location = new System.Drawing.Point(220, 230);
+            this.raps55btn.Name = "raps55btn";
+            this.raps55btn.Size = new System.Drawing.Size(191, 48);
+            this.raps55btn.TabIndex = 6;
+            this.raps55btn.Text = "RAPS 55";
+            this.raps55btn.UseVisualStyleBackColor = false;
+            // 
+            // insats51btn
+            // 
+            this.insats51btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.insats51btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.insats51btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insats51btn.ForeColor = System.Drawing.Color.White;
+            this.insats51btn.Location = new System.Drawing.Point(23, 284);
+            this.insats51btn.Name = "insats51btn";
+            this.insats51btn.Size = new System.Drawing.Size(191, 48);
+            this.insats51btn.TabIndex = 7;
+            this.insats51btn.Text = "INSATS 51";
+            this.insats51btn.UseVisualStyleBackColor = false;
+            // 
+            // insats52btn
+            // 
+            this.insats52btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.insats52btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.insats52btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insats52btn.ForeColor = System.Drawing.Color.White;
+            this.insats52btn.Location = new System.Drawing.Point(220, 284);
+            this.insats52btn.Name = "insats52btn";
+            this.insats52btn.Size = new System.Drawing.Size(191, 48);
+            this.insats52btn.TabIndex = 8;
+            this.insats52btn.Text = "INSATS 52";
+            this.insats52btn.UseVisualStyleBackColor = false;
+            // 
+            // insats53btn
+            // 
+            this.insats53btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.insats53btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.insats53btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insats53btn.ForeColor = System.Drawing.Color.White;
+            this.insats53btn.Location = new System.Drawing.Point(23, 338);
+            this.insats53btn.Name = "insats53btn";
+            this.insats53btn.Size = new System.Drawing.Size(191, 48);
+            this.insats53btn.TabIndex = 9;
+            this.insats53btn.Text = "INSATS 53";
+            this.insats53btn.UseVisualStyleBackColor = false;
+            // 
+            // insats54btn
+            // 
+            this.insats54btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.insats54btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.insats54btn.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insats54btn.ForeColor = System.Drawing.Color.White;
+            this.insats54btn.Location = new System.Drawing.Point(220, 338);
+            this.insats54btn.Name = "insats54btn";
+            this.insats54btn.Size = new System.Drawing.Size(191, 48);
+            this.insats54btn.TabIndex = 10;
+            this.insats54btn.Text = "INSATS 54";
+            this.insats54btn.UseVisualStyleBackColor = false;
+            // 
+            // channelDisplay
+            // 
+            this.channelDisplay.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.channelDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(136)))), ((int)(((byte)(255)))));
+            this.channelDisplay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.channelDisplay.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.channelDisplay.ForeColor = System.Drawing.Color.White;
+            this.channelDisplay.Location = new System.Drawing.Point(23, 9);
+            this.channelDisplay.Name = "channelDisplay";
+            this.channelDisplay.Size = new System.Drawing.Size(388, 29);
+            this.channelDisplay.TabIndex = 11;
+            this.channelDisplay.Text = "Nurvarande Kanal:";
+            this.channelDisplay.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.channelDisplay.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(434, 411);
+            this.Controls.Add(this.channelDisplay);
+            this.Controls.Add(this.insats54btn);
+            this.Controls.Add(this.insats53btn);
+            this.Controls.Add(this.insats52btn);
+            this.Controls.Add(this.insats51btn);
+            this.Controls.Add(this.raps55btn);
+            this.Controls.Add(this.raps54btn);
+            this.Controls.Add(this.raps53btn);
+            this.Controls.Add(this.raps52btn);
+            this.Controls.Add(this.raps51btn);
+            this.Controls.Add(this.raps50btn);
+            this.Controls.Add(this.anropbtn);
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(450, 450);
+            this.MinimumSize = new System.Drawing.Size(450, 450);
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.Text = "SERP - SOS/RLC Rakelsystem";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button anropbtn;
+        private System.Windows.Forms.Button raps50btn;
+        private System.Windows.Forms.Button raps51btn;
+        private System.Windows.Forms.Button raps52btn;
+        private System.Windows.Forms.Button raps53btn;
+        private System.Windows.Forms.Button raps54btn;
+        private System.Windows.Forms.Button raps55btn;
+        private System.Windows.Forms.Button insats51btn;
+        private System.Windows.Forms.Button insats52btn;
+        private System.Windows.Forms.Button insats53btn;
+        private System.Windows.Forms.Button insats54btn;
+        private System.Windows.Forms.Label channelDisplay;
+
+    }
+}
+
